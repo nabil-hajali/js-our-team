78c09126d156bdbef043806a1031125ac12f2ac5
nabil-hajali <nabil2491@gmail.com>
2026-02-18 16:36:37 +0100

    add image property inside array

--- main.js
+++ main.js
@@ -5,38 +5,44 @@ const team = [
     {
         name: 'Marco Bianchi',
         role: 'Designer',
-        mail: 'marcobianchi@team.com'
-        
+        mail: 'marcobianchi@team.com',
+        image: 'male1.png'
+
     },
 
     {
         name: 'Laura Rossi',
         role: 'Front-end Developer',
-        mail: 'laurarossi@team.com'
+        mail: 'laurarossi@team.com',
+        image:'female1.png'
     },
 
     {
         name: 'Marco Bianchi',
         role: 'Designer',
-        mail: 'giorgioverdi@team.com'
+        mail: 'giorgioverdi@team.com',
+        image:'male2.png'
     },
 
     {
         name: 'Marta Ipsum',
         role: 'SEO Specialis',
-        mail: 'martarossi@team.com'
+        mail: 'martarossi@team.com',
+        image:'female2.png'
     },
 
     {
         name: 'Roberto Lorem',
         role: 'SEO Specialist',
-        mail: 'robertolorem@team.com'
+        mail: 'robertolorem@team.com',
+        image:'male3.png'
     },
 
     {
         name: 'Daniela Amet',
         role: 'Analyst',
-        mail: 'danielaamet@team.com'
+        mail: 'danielaamet@team.com',
+        image:'female3.png'
     },
     
 
