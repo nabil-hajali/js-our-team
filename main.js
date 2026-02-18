4000694e8bb476b96506c490278d8f11e03cf492
nabil-hajali <nabil2491@gmail.com>
2026-02-18 16:29:18 +0100

    create array with the team components

---
+++ main.js
@@ -0,0 +1,43 @@
+// Dato un array di oggetti rappresentante un team di un’azienda, creare una pagina dedicata  in cui mostrare una card per ciascun componente.
+// create an array with the components of the team
+
+const team = [
+    {
+        name: 'Marco Bianchi',
+        role: 'Designer',
+        mail: 'marcobianchi@team.com'
+        
+    },
+
+    {
+        name: 'Laura Rossi',
+        role: 'Front-end Developer',
+        mail: 'laurarossi@team.com'
+    },
+
+    {
+        name: 'Marco Bianchi',
+        role: 'Designer',
+        mail: 'giorgioverdi@team.com'
+    },
+
+    {
+        name: 'Marta Ipsum',
+        role: 'SEO Specialis',
+        mail: 'martarossi@team.com'
+    },
+
+    {
+        name: 'Roberto Lorem',
+        role: 'SEO Specialist',
+        mail: 'robertolorem@team.com'
+    },
+
+    {
+        name: 'Daniela Amet',
+        role: 'Analyst',
+        mail: 'danielaamet@team.com'
+    },
+    
+
+]
